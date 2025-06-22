24706dcef27a3c49375e9a0fad65e5c780b8f3e4
PinkKitty100 <bedrockcat100@gmail.com>
2025-06-22 07:34:27 +0200

    Assets are in !

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const witheringWild = Vars.mods.locateMod("withering-wild").meta;
+
+witheringWild.displayName = Core.bundle.get("mod.withering-wild.name");
+witheringWild.author = Core.bundle.get("mod.withering-wild.author");
+witheringWild.description = Core.bundle.get("mod.withering-wild.description");
+witheringWild.subtitle = Core.bundle.get("mod.withering-wild.subtitle-"+String(Math.floor(Math.random()*5)+1));
